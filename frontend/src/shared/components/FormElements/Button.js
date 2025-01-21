da327a50bc1da8490d98444a79dc83125fe23cd3
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-01-21 21:54:07 +0545

    Enhance Button component with improved documentation and consistent formatting

--- frontend/src/shared/components/FormElements/Button.js
+++ frontend/src/shared/components/FormElements/Button.js
@@ -1,43 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './Button.css';
+// Importing CSS styles from Button.css
+import "./Button.css";
 
-const Button = props => {
+// Defining a functional component named Button
+const Button = (props) => {
+  // If the href property is provided, render an <a> element
   if (props.href) {
     return (
+      // Adding class names based on props
       <a
-        className={`button button--${props.size || 'default'} ${props.inverse &&
-          'button--inverse'} ${props.danger && 'button--danger'}`}
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
+        // Using the href property from props
         href={props.href}
       >
+        // Rendering the children prop
         {props.children}
       </a>
     );
   }
+  // If the to property is provided, render a <Link> element from react-router-dom
   if (props.to) {
     return (
       <Link
+        // Using the to property from props
         to={props.to}
+        // Using the exact property from props, if provided
         exact={props.exact}
-        className={`button button--${props.size || 'default'} ${props.inverse &&
-          'button--inverse'} ${props.danger && 'button--danger'}`}
+        // Adding class names based on props
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
       >
+        // Rendering the children prop
         {props.children}
       </Link>
     );
   }
+  // If neither href nor to is provided, render a <button> element
   return (
     <button
-      className={`button button--${props.size || 'default'} ${props.inverse &&
-        'button--inverse'} ${props.danger && 'button--danger'}`}
+      // Adding class names based on props
+      className={`button button--${props.size || "default"} ${
+        props.inverse && "button--inverse"
+      } ${props.danger && "button--danger"}`}
+      // Using the type property from props, if provided
       type={props.type}
+      // Using the onClick property from props, if provided
       onClick={props.onClick}
+      // Using the disabled property from props, if provided
       disabled={props.disabled}
     >
+      {/* // Rendering the children prop */}
       {props.children}
     </button>
   );
 };
 
+// Exporting the Button component as the default export
 export default Button;
